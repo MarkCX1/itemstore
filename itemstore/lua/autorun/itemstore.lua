52838f2fcd820126349ea51e5e1bc3bb239192d3
MarkCX <49326294+MarkCX1@users.noreply.github.com>
2026-03-16 18:13:52 -0400

    Delete itemstore directory

--- itemstore/lua/autorun/itemstore.lua
+++
@@ -1,9 +0,0 @@
-hook.Add( "PostGamemodeLoaded", "ItemStoreInitialize", function()
-	itemstore = {}
-
-	if SERVER then
-		include( "itemstore/sv_init.lua" )
-	else
-		include( "itemstore/cl_init.lua" )
-	end
-end )
--- itemstore/lua/itemstore/cl_init.lua
+++
@@ -1,4 +0,0 @@
-include( "shared.lua" )
-
-include( "cl_gui.lua" )
-include( "cl_player.lua" )
